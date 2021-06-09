069c7439266629feb0c62a4a420a8a25b6358e89
Sujith NS <sujit.nss@gmail.com>
2021-06-09 10:15:31 +0200

    Add files via upload
    test desc

---
+++ population.py
@@ -0,0 +1,22 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
+from sklearn.naive_bayes import GaussianNB
+
+
+df = pd.read_csv('Berlin-population-2020-10-17.csv')
+df = df.dropna()
+
+y  = df['Annual Change'].values
+X  = df.iloc[:,:-1]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
+gnb = GaussianNB()
+y_pred = gnb.fit(X_train, y_train).predict(X_test)
+
+
+print('F1 Score: ', metrics.f1_score(y_test, y_pred, average='macro'))
+print('Accuracy: ', metrics.accuracy_score(y_test, y_pred,average='macro'))
+
+
+
